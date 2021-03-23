a25c65024f169e36921ecae79ecd8f95be023eb8
Kokande <sdobrovna@gmail.com>
2021-03-24 01:09:18 +0500

    The beggining of the site! Made beautiful navbar and logo(might change later). Fix 1

--- main.py
+++ main.py
@@ -10,42 +10,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'notsosecretkey'
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     return render_template("main.html", title="Welcome! | NetWorks")
 
 
-@app.route("/model1")
+@app.route("/model1", methods=['GET'])
 def model1():
     return render_template("model1.html", title="Wider Model | NetWorks")
 
 
-@app.route("/model2")
+@app.route("/model2", methods=['GET'])
 def model2():
     return render_template("model2.html", title="Model 2 | NetWorks")
 
 
-@app.route("/model3")
+@app.route("/model3", methods=['GET'])
 def model3():
     return render_template("model3.html", title="Model 3 | NetWorks")
 
 
-@app.route("/main_model")
+@app.route("/main_model", methods=['GET'])
 def main_model():
     return render_template("main_model.html", title="Main Model | NetWorks")
 
 
-@app.route("/models")
+@app.route("/models", methods=['GET'])
 def models():
     return render_template("main.html", title="Models | NetWorks")
 
 
-@app.route("/contacts")
+@app.route("/contacts", methods=['GET'])
 def contacts():
     return render_template("contacts.html", title="Contacts | NetWorks")
 
 
-@app.route("/about")
+@app.route("/about", methods=['GET'])
 def about():
     return render_template("about.html", title="About | NetWorks")
 
